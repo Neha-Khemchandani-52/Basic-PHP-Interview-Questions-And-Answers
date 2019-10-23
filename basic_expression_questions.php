4389b8ca89e5c494e06317691a25209dbd413d38
Neha Khemchandani <42884876+Neha-Khemchandani-52@users.noreply.github.com>
2019-10-23 13:10:21 +0530

    Added File

---
+++ basic_expression_questions.php
@@ -0,0 +1,59 @@
+<?php 
+/***
+ * Description : Basic PHP Interview Questions And Answers
+ * Added By : Neha Khemchandani
+ * Date Added : 23rd October,2019
+ */
+
+    echo '5'+8;     ## Output 13
+    echo "<br>";
+
+    echo '5'+'8';   ## Output 13
+    echo "<br>";
+
+    echo 5+'8';     ## Output 13
+    echo "<br>";
+
+    echo 5+8;       ## Output 13
+    echo "<br>";
+
+    echo '5+8';     ## Output 5+8
+    echo "<br>";
+
+    echo "5+8";     ## Output 5+8
+    echo "<br>";
+
+
+    $num  = 1;
+    $num1 = 2;
+    print $num . "+". $num1;  ## Output 1+2
+    echo "<br>";
+
+    $num  = "1";
+    $num1 = "2";
+    print $num+$num1;   ## Output 3
+    echo "<br>";
+
+    $a=5;
+    $b= 8;
+    echo "$a"+"$b";    ## Output 13 
+    echo "<br>";
+
+    echo '$a'+'$b';    ## output will be 0
+    echo "<br>";
+
+    
+    $total = "25 students";
+    $more = 10;
+    $total = $total + $more;
+    echo "$total";             ## Output will be 35
+    echo "<br>";
+
+    $total = "students 25";
+    $more = 10;
+    $total = $total + $more;
+    echo "$total";             ## Output will be 10
+
+
+
+    ?>
